6d865e2d3310a2d0c4891d955474575563b0ef8e
jason-zc-xue <59970170+jason-zc-xue@users.noreply.github.com>
2024-11-12 17:42:52 -0500

    Some additions to localization.py
    Filled out some ROS parts but not finished

--- localization.py
+++ localization.py
@@ -47,19 +47,19 @@ class localization(Node):
         
         # TODO Part 3: Set up the quantities for the EKF (hint: you will need the functions for the states and measurements)
         
-        x= ...
+        x= np.array([0, 0, 0, 0, 0, 0]) # x, y, th, w, v, vdot
         
-        Q= ...
+        Q= 0.5 * np.eye(6)
 
-        R= ...
+        R= 0.5 * np.eye(4)
         
         P= ... # initial covariance
         
         self.kf=kalman_filter(P,Q,R, x, dt)
         
         # TODO Part 3: Use the odometry and IMU data for the EKF
-        self.odom_sub=message_filters.Subscriber(...)
-        self.imu_sub=message_filters.Subscriber(...)
+        self.odom_sub=message_filters.Subscriber("/odom", odom)
+        self.imu_sub=message_filters.Subscriber("/imu", imu)
         
         time_syncher=message_filters.ApproximateTimeSynchronizer([self.odom_sub, self.imu_sub], queue_size=10, slop=0.1)
         time_syncher.registerCallback(self.fusion_callback)
@@ -71,10 +71,16 @@ class localization(Node):
         # your measurements are the linear velocity and angular velocity from odom msg
         # and linear acceleration in x and y from the imu msg
         # the kalman filter should do a proper integration to provide x,y and filter ax,ay
-        z=...
+        z=np.array([
+            ..., # v
+            ..., # w
+            ..., # ax
+            ...  # ay
+        ])
         
         # Implement the two steps for estimation
-        ...
+        self.kf.predict()
+        self.kf.update(z)
         
         # Get the estimate
         xhat=self.kf.get_states()
