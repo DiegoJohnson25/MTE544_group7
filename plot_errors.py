435ba9b2591d9e7d69c793af341a4af9ac1b2048
Henchel Santillan <henchelsantillan@gmail.com>
2024-12-03 21:13:44 -0500

    Add plotting file to lab report

--- plot_errors.py
+++ plot_errors.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
+import matplotlib.image as img
 from utilities import FileReader
 
-def plot_errors():
-    
-    headers, values=FileReader("robot_pose.csv").read_file()
+import argparse
+import yaml
 
-    
+def plot_errors(file, map_file, distance_fn):
+    headers, values=FileReader(file).read_file()
+
     time_list=[]
     
     first_stamp=values[0][-1]
@@ -13,17 +15,35 @@ def plot_errors():
     for val in values:
         time_list.append(val[-1] - first_stamp)
 
-
-
-    for i in range(0, len(headers) - 1):
-        plt.plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
-
-    plt.legend()
+    plt.title(f"{distance_fn}: Trajectory, Actual")
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
     plt.grid()
 
+    plt.plot([lin[0] for lin in values], [lin[1] for lin in values])
+
+    with open(map_file, "r") as file:
+        metadata = yaml.safe_load(file)
+
+        res, origin, path = metadata["resolution"], metadata["origin"], metadata["image"]
+        map_image = img.imread(path)
+
+        # Return extent of map in world coordinates
+        extent = [origin[0], origin[0] + map_image.shape[1] * res, origin[1], origin[1] + map_image.shape[0] * res]
+        plt.imshow(map_image, cmap="gray", origin="upper", extent=extent)
+
     plt.show()
-    
-    
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file",required=True, help="Pose CSV file")
+    parser.add_argument("--map", required=True, help="Map YAML file")
+    parser.add_argument("--dist", required=True, help="Distance function used (Manhattan or Euclidean)")
+
+    args = parser.parse_args()
+
+    plot_errors(args.file, args.map, args.dist)
+
 
 if __name__=="__main__":
-    plot_errors()
+    main()
